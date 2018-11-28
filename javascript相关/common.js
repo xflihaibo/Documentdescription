6244e43e2099c62d3fb82382975e0f20929b13b5
Silence520 <1056497547@qq.com>
2018-11-28 15:16:22 +0800

    updata code

--- javascript相关/common.js
+++ javascript相关/common.js
@@ -71,6 +71,29 @@ export const tmsBack = () => {
 };
 
 /**
+ * [GetUrlParam description] 获取url 参数值
+ * @type {[type]} URL 参数地址
+ */
+export const GetUrlParam = paraName => {
+    var url = document.location.toString();
+    var arrObj = url.split('?');
+    if (arrObj.length > 1) {
+        var arrPara = arrObj[1].split('&');
+        var arr;
+        for (var i = 0; i < arrPara.length; i++) {
+            arr = arrPara[i].split('=');
+
+            if (arr != null && arr[0] == paraName) {
+                return arr[1];
+            }
+        }
+        return '';
+    } else {
+        return '';
+    }
+};
+
+/**
  * 设置 cookie
  * @constructor tmsSetCookie
  * @param {string} name - key值.
